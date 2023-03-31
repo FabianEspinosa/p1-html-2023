8d8c0e1570baa8fb21222310e7ca1f0f8f5617f7
Fabian Espinosa <fabianespinosa1988@gmail.com>
2023-03-31 22:20:19 +0200

    slider fade in/out

--- script.js
+++ script.js
@@ -16,7 +16,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const totalSlides = slides.length
   const prevBtn = document.querySelector('#prevBtn')
   const nextBtn = document.querySelector('#nextBtn')
-  const showSlides = (n) => {
+  const disableButton = () => {
+    nextBtn.style.pointerEvents = 'none'
+    prevBtn.style.pointerEvents = 'none'
+    setTimeout(() => {
+      nextBtn.style.pointerEvents = 'auto'
+      prevBtn.style.pointerEvents = 'auto'
+    }, 1500)
+  }
+  const showSlides = (n, btn) => {
+    disableButton()
     if (n > totalSlides) {
       slideIndex = 1
     } else if (n < 1) {
@@ -24,19 +33,54 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
       slideIndex = n
     }
-    for (let i = 0; i < totalSlides; i++) {
-      slides[i].style.display = 'none'
+    console.log(slideIndex)
+    console.log(n)
+    if (btn === 'nextBtn') {
+      if (n > totalSlides) {
+        slides[0].style.zIndex = 2
+        slides[totalSlides - 1].style.zIndex = null
+        setTimeout(() => {
+          slides[totalSlides - 1].classList.remove('show')
+        }, 1500)
+        slides[slideIndex - 1].classList.add('show')
+      } else {
+        slides[slideIndex - 2].style.zIndex = null
+        setTimeout(() => {
+          slides[slideIndex - 2].classList.remove('show')
+        }, 1500)
+        slides[slideIndex - 1].classList.add('show')
+      }
+    } else if (btn === 'prevBtn') {
+      if (n < 1) {
+        slides[0].style.zIndex = null
+        setTimeout(() => {
+          slides[0].classList.remove('show')
+        }, 1500)
+        slides[totalSlides - 1].classList.add('show')
+        slides[totalSlides - 1].style.zIndex = 2
+      } else {
+        slides[slideIndex].style.zIndex = null
+        setTimeout(() => {
+          slides[slideIndex].classList.remove('show')
+        }, 1500)
+        slides[slideIndex - 1].classList.add('show')
+        slides[slideIndex - 1].style.zIndex = 2
+      }
+    } else if (btn === 'new') {
+      slides[slideIndex - 1].classList.add('show')
     }
-    slides[slideIndex - 1].style.display = 'block'
   }
-  const plusSlides = (n) => {
-    showSlides((slideIndex += n))
+  const plusSlides = (n, btn) => {
+    showSlides((slideIndex += n), btn)
   }
   prevBtn.addEventListener('click', function () {
-    plusSlides(-1)
+    plusSlides(-1, 'prevBtn')
   })
   nextBtn.addEventListener('click', function () {
-    plusSlides(1)
+    plusSlides(1, 'nextBtn')
   })
-  showSlides(slideIndex)
+  showSlides(slideIndex, 'new')
+  setInterval(() => {
+    plusSlides(1, 'nextBtn')
+  }, 10000)
 })
